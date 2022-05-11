e5df88840705a0c0558f90f609a473f0dfdb2cb8
Jimmy Simbaña <75291241+JimmySimbana99@users.noreply.github.com>
2022-05-11 00:47:25 -0500

    Aplicativo

---
+++ PARCIAL I/Aplicativo insertar/Ingresar.h
@@ -0,0 +1,21 @@
+/*	UNIVERSITY OF THE ARMED FORCES ESPE
+	COMPUTER SCIENCE DEPARTMENT
+    SOFTWARE ENGINEER
+                
+	AUTHORS:   	ALEXANDER MAILA - jamaila@espe.edu.ec
+				JIMMY SIMBAÑA - jasimbana14@espe.edu.ec
+	
+	STATEMENT: 	CHANGE STRING TO INTEGER		
+				
+	CREATION DATE:        10-05-22 	
+	MODIFICATION DATE:    10-05-22*/
+	
+//LIBRARY STATEMENT	
+#include <conio.h>
+#include <stdio.h>
+#include <string>
+using namespace std;
+class Ingresar {
+	public:
+		void ingresar(char*);
+};
---
+++ PARCIAL I/Aplicativo insertar/main.cpp
@@ -0,0 +1,22 @@
+/*	UNIVERSITY OF THE ARMED FORCES ESPE
+	COMPUTER SCIENCE DEPARTMENT
+    SOFTWARE ENGINEER
+                
+	AUTHORS:   	ALEXANDER MAILA - jamaila@espe.edu.ec
+				JIMMY SIMBAÑA - jasimbana14@espe.edu.ec
+	
+	STATEMENT: 	CHANGE STRING TO INTEGER		
+				
+	CREATION DATE:        10-05-22 	
+	MODIFICATION DATE:    10-05-22*/
+	
+//LIBRARY STATEMENT	
+#include <iostream>
+#include "Ingresar.h"
+using namespace std;
+
+int main()
+{
+    Ingresar ingreso;
+    ingreso.ingresar("ingrese un numero");
+}
