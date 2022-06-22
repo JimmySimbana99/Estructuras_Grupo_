360d90f3c9309a209602644edd6c182e4e741abc
Jimmy Simbaña <75291241+JimmySimbana99@users.noreply.github.com>
2022-06-22 07:33:42 -0500

    Busqueda codigo

---
+++ PARCIAL II/Deber2_Algoritmo_secuencial/Codigo/Busqueda.h
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class Busqueda{
+    private:
+    int *datos;
+
+    public:
+      void ingresarElementos(int);
+      void buscarElemento(int,int);
+};
+
